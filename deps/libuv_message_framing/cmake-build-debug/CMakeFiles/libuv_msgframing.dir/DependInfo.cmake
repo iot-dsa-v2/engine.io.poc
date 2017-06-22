f72e61fe7ec452df62806dd279adeb4de7147e29
rkumar-1 <ravi.mohandas@acuitybrands.com>
2017-06-22 11:58:48 -0700

    Add files via upload

---
+++ deps/libuv_message_framing/cmake-build-debug/CMakeFiles/libuv_msgframing.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/ravi/projects/iot-dsa-v2/deps/libuv_message_framing-master/src/uv_msg_framing.c" "/home/ravi/projects/iot-dsa-v2/deps/libuv_message_framing-master/cmake-build-debug/CMakeFiles/libuv_msgframing.dir/src/uv_msg_framing.c.o"
+  "/home/ravi/projects/iot-dsa-v2/deps/libuv_message_framing-master/src/uv_send_message.c" "/home/ravi/projects/iot-dsa-v2/deps/libuv_message_framing-master/cmake-build-debug/CMakeFiles/libuv_msgframing.dir/src/uv_send_message.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/ravi/projects/iot-dsa-v2/deps/libuv-1.x/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ deps/libuv_message_framing/cmake-build-debug/CMakeFiles/libuv_msgframing.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libuv_msgframing.dir/src/uv_msg_framing.c.o"
+  "CMakeFiles/libuv_msgframing.dir/src/uv_send_message.c.o"
+  "liblibuv_msgframing.pdb"
+  "liblibuv_msgframing.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/libuv_msgframing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
